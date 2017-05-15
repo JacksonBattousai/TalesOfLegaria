440efcaf0743ea0a68db9c897261829669950ef8
JacksonBattousai <oiitau@live.com>
2017-05-15 20:57:57 -0300

    .

---
+++ TalesOfLegariaUnity/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuScript : MonoBehaviour
+{
+
+    private void OnGUI()
+    {
+        const int buttonWidth = 100;
+        const int buttonHeight = 60;
+
+        if (GUI.Button(
+            new Rect(Screen.width / 15 - (buttonWidth / 2),
+                    (2 * Screen.height / 3) - (buttonWidth / 2),
+                    buttonWidth, buttonHeight),
+            "Start!"))
+        {
+            Application.LoadLevel("Nome da Cena a transitar");
+        }
+
+
+
+
+    }
+
+
+
+
+
+
+}

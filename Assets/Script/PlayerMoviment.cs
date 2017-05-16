295c71b0d5d1a5bdc2aa27d5d6d19be65d704f0e
necofrago <filipedecastroresende@gmail.com>
2017-05-16 16:48:07 -0300

    modificação 16/05/2017 - 16:47
    >mapa seguindo personagem
    >adição da caverna.

---
+++ Assets/Script/PlayerMoviment.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoviment : MonoBehaviour {
+
+Rigidbody2D rbody;
+ Animator anim;
+	// Use this for initialization
+	void Start () {
+		
+		rbody = GetComponent<Rigidbody2D>();
+		anim = GetComponent<Animator>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+
+
+		Vector2 moviment_vector = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
+
+		if(moviment_vector!= Vector2.zero){
+			anim.SetBool ("iswalking",true);
+			anim.SetFloat ("input_x", moviment_vector.x);
+			anim.SetFloat ("input_y", moviment_vector.y);
+
+		} else{
+			
+			anim.SetBool ("iswalking", false); 
+		}
+
+		rbody.MovePosition (rbody.position + moviment_vector * Time.deltaTime*70);
+
+}
+
+
+}

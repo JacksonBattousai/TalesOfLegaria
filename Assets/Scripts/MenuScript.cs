3c9eb2976c6084dc15d12f7cd12b2a68afe5b696
necofrago <filipedecastroresende@gmail.com>
2017-06-18 20:43:33 -0300

    NOVO MENU
    NOVO MENU

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -5,28 +5,14 @@ using UnityEngine;
 public class MenuScript : MonoBehaviour
 {
 
-    private void OnGUI()
-    {
-        const int buttonWidth = 100;
-        const int buttonHeight = 60;
-
-        if (GUI.Button(
-            new Rect(Screen.width / 15 - (buttonWidth / 2),
-                    (2 * Screen.height / 3) - (buttonWidth / 2),
-                    buttonWidth, buttonHeight),
-            "Start!"))
-        {
+    public void NewGame()    
+		{
             Application.LoadLevel("teste");
         }
 
-
-
-
-    }
-
-
-
-
-
+	public void Sair(){
+	
+		Application.Quit();
+	}
 
 }
